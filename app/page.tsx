9977977d961b43ede06fa35e2d2ba7ea1394a9bc
Ramazan Ünal <unalramaz@gmail.com>
2025-07-29 14:30:25 +0300

    status db connection made

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   onSnapshot,
   doc,
   deleteDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "@/firestore";
 
@@ -44,25 +45,24 @@ export default function Home() {
     await deleteDoc(doc(db, "todo", id));
   };
   // todo add firebase
-  const toggleStatus = (id) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, status: !todo.status } : todo
-      )
-    );
+  const toggleStatus = async(id:string,status:boolean) => {
+    const ref = doc(db,"todo",id)
+    await updateDoc(ref,{
+      status:!status
+    })
   };
 
   return (
     <main className="min-h-screen bg-gray-100 p-4 md:py-10 flex flex-col border items-center">
       <h1 className="text-2xl font-bold mb-4">📋 Todo List</h1>
 
-      <div className="flex gap-2 mb-4">
+      <div className="flex gap-2 mb-4 w-full justify-center">
         <input
           maxLength={100}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Yapılacak iş..."
-          className="p-2 border border-gray-300 rounded"
+          className="p-2 border border-gray-300 rounded w-2/5"
         />
         <button
           onClick={addTodo}
@@ -89,7 +89,7 @@ export default function Home() {
                     <div className="break-words w-4/5">{todo.todo}</div>
                     <div className="w-1/5 flex justify-evenly">
                       <button
-                        onClick={() => toggleStatus(todo.id)}
+                        onClick={() => toggleStatus(todo.id,todo.status)}
                         className="text-green-500 p-2 text-xl hover:bg-green-200 transition-all ease-in rounded-full cursor-pointer"
                       >
                         <PiCheckCircleBold />
@@ -121,7 +121,7 @@ export default function Home() {
                     <div className="break-words w-4/5">{todo.todo}</div>
                     <div className="w-1/5 flex justify-evenly">
                       <button
-                        onClick={() => toggleStatus(todo.id)}
+                        onClick={() => toggleStatus(todo.id,todo.status)}
                         className="text-green-500 p-2 text-xl hover:bg-green-300 transition-all ease-in rounded-full cursor-pointer"
                       >
                         <PiCheckCircleFill />
