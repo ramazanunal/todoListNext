25ab1f6f6c3a257a09ca943548c8abc9de4b6e85
Ramazan Ünal <unalramaz@gmail.com>
2025-09-23 16:24:01 +0300

    firestore functions done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+"use client";
+
 import Login from "@/components/Login";
 import SignOut from "@/components/SignOut";
+import {
+  newTodo,
+  TodoItem,
+  getAllData,
+  removeTodoItem,
+  removeTodo,
+  updateTodo,
+} from "@/firebase";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<TodoItem[]>([]);
+
+  const todos: TodoItem = {
+    title: "Alışveriş Listesi",
+    completed: false,
+    children: [
+      { text: "Ekmek al", completed: false },
+      { text: "Süt al", completed: true },
+    ],
+  };
+  useEffect(() => {
+    getAllData().then((result) => {
+      if (result.length > 0) {
+        setData(result);
+      }
+    });
+    //newTodo(todos);
+    //setData((data)=>{ data = getAllData()})
+  }, []);
+
+  useEffect(() => {
+    console.log("data güncellendi:", data);
+    if (data.length > 0) {
+      //removeTodo(data[0].id)
+      const payload: TodoItem = {
+        id: data[0].id,
+        title: "this has been changed",
+        completed: true,
+        children: [
+          { text: "Ekmek al", completed: false },
+          { text: "Süt al", completed: true },
+          { text: "Ekmesk al", completed: false },
+          { text: "Süt asdsdsdl", completed: true },
+          { text: "Ekmsdek al", completed: false },
+          { text: "Süt sdsdal", completed: true },
+        ],
+      };
+      updateTodo(data[0].id, payload);
+    }
+  }, [data]);
+
   return (
     <div className="flex justify-between ">
       <h1>hello world</h1>
--- firebase.ts
+++ firebase.ts
@@ -10,7 +10,17 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  setDoc,
+  doc,
+  getDoc,
+  onSnapshot,
+  deleteDoc,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -31,7 +41,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// auth language responsive to browser lang
+// auth language is responsive to browser lang
 auth.useDeviceLanguage();
 export function signout() {
   const auth = getAuth();
@@ -39,12 +49,10 @@ export function signout() {
     .then(() => {
       // Sign-out successful.
       console.log("sign out successfull");
-      
     })
     .catch((error) => {
       // An error happened.
       console.error("An error happened.");
-      
     });
 }
 export function signInPop() {
@@ -56,7 +64,7 @@ export function signInPop() {
       // The signed-in user info.
       const user = result.user;
       console.log(user);
-      
+
       // IdP data available using getAdditionalUserInfo(result)
       // ...
     })
@@ -72,4 +80,97 @@ export function signInPop() {
     });
 }
 
-const firestore = getFirestore(app);
+const db = getFirestore(app);
+
+export type TodoItemChildren = {
+  text: string;
+  completed: boolean;
+};
+
+export type TodoItem = {
+  id: string;
+  title: string;
+  completed: boolean;
+  children: Array<TodoItemChildren>;
+};
+// it can also delete child todo items be careful with it!
+export async function updateTodo(todoItemId: string, newTodoItem: TodoItem) {
+  await setDoc(doc(db, "todos", todoItemId), newTodoItem);
+}
+
+export async function newTodo(todoItem: TodoItem) {
+  // Add a new document with a generated id.
+  const docRef = await addDoc(collection(db, "todos"), todoItem);
+  console.log("Document written with ID: ", docRef.id);
+}
+
+export async function getAllDataRealtime() {
+  const unsub = onSnapshot(doc(db, "todos"), (doc) => {
+    console.log("Current data: ", doc.data());
+  });
+}
+
+export async function getAllData() {
+  const querySnapshot = await getDocs(collection(db, "todos"));
+  const data: Array<TodoItem> = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    const payload: TodoItem = {
+      id: doc.id,
+      title: doc.data().title,
+      completed: doc.data().completed,
+      children: doc.data().children,
+    };
+    data.push(payload);
+  });
+  return data;
+}
+
+export async function removeTodo(id: string) {
+  await deleteDoc(doc(db, "todos", id));
+}
+
+export async function removeTodoItem(
+  todoItem: TodoItem,
+  todoChildText: string
+) {
+  const resultChild = todoItem.children.filter((child) => {
+    console.log(child.text, todoChildText);
+
+    console.log("sonuç", child.text === todoChildText);
+
+    return child.text !== todoChildText;
+  });
+  console.log("outside", resultChild);
+
+  todoItem.children = resultChild;
+
+  await setDoc(doc(db, "todos", todoItem.id), todoItem);
+}
+
+const todos: TodoItem[] = [
+  {
+    title: "Alışveriş Listesi",
+    completed: false,
+    children: [
+      { text: "Ekmek al", completed: false },
+      { text: "Süt al", completed: true },
+    ],
+  },
+  {
+    title: "Projeyi Tamamla",
+    completed: false,
+    children: [
+      { text: "Backend API yaz", completed: false },
+      { text: "UI tasarla", completed: false },
+    ],
+  },
+  {
+    title: "Günlük Rutin",
+    completed: true,
+    children: [
+      { text: "Sabah sporu", completed: true },
+      { text: "Kitap oku", completed: true },
+    ],
+  },
+];
